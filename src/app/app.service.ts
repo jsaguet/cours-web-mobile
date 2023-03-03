a7b0fe0cedd452c10afd1722597d4122b99930ab
Julien Saguet <julien.saguet@outlook.fr>
2023-03-03 10:38:30 +0100

    feat: http client example

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Joke {
+  setup: string;
+
+  delivery: string;
+}
 
 @Injectable({ providedIn: 'root' })
 export class AppService {
-  private counterSubject = new BehaviorSubject<number>(1);
-  counter$: Observable<number> = this.counterSubject.asObservable();
+  constructor(private httpClient: HttpClient) {}
 
-  public emitNewCounterValue(value: number) {
-    this.counterSubject.next(value);
+  public getJoke(): Observable<Joke> {
+    return this.httpClient.get<Joke>(
+      'https://v2.jokeapi.dev/joke/Programming?type=twopart&safe-mode'
+    );
   }
 }
