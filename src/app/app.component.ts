96b02ad7425e7f096be01239265d6375f19b43ba
Julien Saguet <julien.saguet@outlook.fr>
2023-03-03 13:57:29 +0100

    like and next jokes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
-import { AppService } from './app.service';
-import { AsyncPipe, NgIf } from '@angular/common';
+import { Joke, JokeService } from './joke.service';
+import { AsyncPipe, NgForOf, NgIf } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { JokeComponent } from './joke/joke.component';
 
 @Component({
   standalone: true,
-  imports: [AsyncPipe, NgIf],
-  selector: 'cours-web-mobile-root',
+  imports: [AsyncPipe, NgIf, RouterOutlet, JokeComponent, NgForOf],
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  joke$ = this.appService.getJoke();
+  public readonly joke$ = this.jokeService.joke$;
 
-  constructor(private readonly appService: AppService) {}
+  public readonly likedJokes$ = this.jokeService.likedJokes$;
+
+  constructor(private readonly jokeService: JokeService) {}
+
+  public nextJoke(): void {
+    this.jokeService.nextJoke();
+  }
+
+  public jokeLiked(joke: Joke): void {
+    this.jokeService.jokeLiked(joke);
+  }
 }
