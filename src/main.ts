407ab157edfee3c2890ceec08d58b5b57352ab98
Julien Saguet <julien.saguet@outlook.fr>
2023-03-04 22:19:24 +0100

    use angular fire compat API

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { importProvidersFrom, isDevMode } from '@angular/core';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from './environments/environment';
-import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { getStorage, provideStorage } from '@angular/fire/storage';
-import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -17,12 +16,9 @@ bootstrapApplication(AppComponent, {
         registrationStrategy: 'registerWhenStable:30000',
       })
     ),
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase))
-    ),
-    importProvidersFrom(provideAuth(() => getAuth())),
-    importProvidersFrom(provideFirestore(() => getFirestore())),
-    importProvidersFrom(provideStorage(() => getStorage())),
-    { provide: FIREBASE_OPTIONS, useValue: environment.firebase },
+    importProvidersFrom(AngularFireModule.initializeApp(environment.firebase)),
+    importProvidersFrom(AngularFireAuthModule),
+    importProvidersFrom(AngularFirestoreModule),
+    importProvidersFrom(AngularFireStorageModule),
   ],
 }).catch((err) => console.error(err));
